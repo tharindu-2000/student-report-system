a5344e03f62f2fab4243bc5ae32a73cf7c306a33
LahiruCW <as2021356@sci.sjp.ac.lk>
2025-02-16 15:42:33 +0530

    Initialize the back end

---
+++ back-end/src/main/java/com/ranasingheschl/stureport/StureportApplication.java
@@ -0,0 +1,13 @@
+package com.ranasingheschl.stureport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StureportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StureportApplication.class, args);
+	}
+
+}
